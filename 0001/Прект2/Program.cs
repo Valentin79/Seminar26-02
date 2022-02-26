13380b8fcfc113befa24dd840c076d3ee551fdb7
Valentin <zvezdochet.79@mail.ru>
2022-02-26 11:02:42 +0300

    Проект2

---
+++ 0001/Прект2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+string a = Console.ReadLine();
+int aa = int.Parse(a);
+Console.WriteLine("Введите второе число: ");
+string b = Console.ReadLine();
+int bb = int.Parse(b);
+Console.WriteLine($"{aa}, {bb}");
+int result = aa*aa;
+
+if (result == bb)
+{
+    Console.WriteLine("Верно");
+}
+else
+{
+    Console.WriteLine("Неверно");
+}
