d94d286dadc62959d276af4df6050abb6e716a0a
Valentin <zvezdochet.79@mail.ru>
2022-02-26 12:03:01 +0300

    Начал проект3

---
+++ 0001/Проект3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите номер от 1 до 7: ");
+string n = Console.ReadLine();
+int number = int.Parse(n);
+if (number == 1);
+{
+    Console.WriteLine("Понедельник");
+
+    
+    if (number == 2)
+    {
+        Console.WriteLine("Вторник");
+
+        
+        if (number == 3)
+        {
+            Console.WriteLine("Среда");
+        
+        }
+    }
+}
